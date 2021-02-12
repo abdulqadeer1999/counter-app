65cb6f6c107c34b7a4ceb99836b3d9fcd13bc0b4
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-02-12 15:43:36 +0500

    react bootstrap buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 
 
@@ -25,11 +26,14 @@ function Count() {
 
 
       {/* <button className= "btn" onClick={add}>Increase</button> */}
-      <button type="button" class="btn btn-success" onClick = {add}>Increase</button>
+      {/* <button type="button" class="btn btn-success" onClick = {add}>Increase</button> */}
+      <Button variant="outline-success" onClick = {add} >Increase</Button>{' '} 
       <h1 > {value}   </h1>
 
       {/* <button className="btn1" onClick={sub}>Decrease</button> */}
-      <button type="button" class="btn btn-danger" onClick = {sub}>Decrease</button>
+      {/* <button type="button" class="btn btn-danger" onClick = {sub}>Decrease</button> */}
+
+      <Button variant="outline-danger" onClick = {sub}>Decrease</Button>{' '}
 
     </div>
   );
